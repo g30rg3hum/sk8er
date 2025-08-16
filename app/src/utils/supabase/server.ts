fcddb67f1dcea9164cf0e456bebd6815568ffe5e
Weng Wong (George) Hum <124537582+wengwongg@users.noreply.github.com>
2025-08-16 17:05:24 +0800

    setup: added supabase dependencies and env variables
    - Able to query from Server Components now

---
+++ app/src/utils/supabase/server.ts
@@ -0,0 +1,37 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export async function createClient() {
+  const cookieStore = await cookies();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        // Reading auth cookies
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          // Called from a Server Component, read-only, can't modify cookies
+          // Updating the auth cookies with new tokens
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
+          } catch (error) {
+            // Setting cookies should fail silently
+            // TODO: Need middleware to refresh tokens [SK8-6]
+            if (process.env.NODE_ENV === "development") {
+              console.warn(
+                "Cookie setting failed in Server Component (this is expected):",
+                error
+              );
+            }
+          }
+        },
+      },
+    }
+  );
+}
