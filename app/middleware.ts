b555683a2edd73006dcc505c40c8202e91f76131
Weng Wong (George) Hum <124537582+wengwongg@users.noreply.github.com>
2025-08-26 13:53:58 +0800

    build: reverted middleware

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,10 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
-// import { updateSession } from "./src/utils/supabase/middleware";
+import { NextRequest } from "next/server";
+import { updateSession } from "./src/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
   // uses middleware function for Supabase
-  // return await updateSession(request);
-  return NextResponse.next();
+  return await updateSession(request);
 }
 
 export const config = {
